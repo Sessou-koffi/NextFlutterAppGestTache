eca134c872a3cd21f1947b13016dde1a1bd71f3e
Sessou-koffi <gaelsessou857@gmail.com>
2026-07-09 16:29:20 +0100

    Initialisation forcée de la structure Dart pure

---
+++ bin/next_flutter_app_gest_tache.dart
@@ -0,0 +1,5 @@
+import 'package:next_flutter_app_gest_tache/next_flutter_app_gest_tache.dart' as next_flutter_app_gest_tache;
+
+void main(List<String> arguments) {
+  print('Hello world: ${next_flutter_app_gest_tache.calculate()}!');
+}
---
+++ test/next_flutter_app_gest_tache_test.dart
@@ -0,0 +1,8 @@
+import 'package:next_flutter_app_gest_tache/next_flutter_app_gest_tache.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
